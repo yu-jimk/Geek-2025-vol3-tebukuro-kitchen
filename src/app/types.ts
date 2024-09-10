7d0e2817caac267fc6d10278329d239d410cae8e
watagassa <k23095kk@aitech.ac.jp>
2024-09-10 14:50:03 +0900

    fix:フォーマット

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,24 +2,23 @@
 //     id: number;
 //     title: string;
 //   }
-  export type  Recipe = {
-    id: number;
-    name: string;
-      image_url?: string;
-      time?: number;//料理時間
-      comment?: string;
-      howmany: string;//何人前
-  }
-  export type Ingredient = {
-    id: number;
-    recipe_id: number;
-    name: string;
-    amount: string;
-  }
-  export type Descript ={
-    id: number;
-    recipe_id: number;
-    image_url:string;
-    text: string;
-  }
-  
+export type Recipe = {
+  id: number;
+  name: string;
+  image_url?: string;
+  time?: number; //料理時間
+  comment?: string;
+  howmany: string; //何人前
+};
+export type Ingredient = {
+  id: number;
+  recipe_id: number;
+  name: string;
+  amount: string;
+};
+export type Descript = {
+  id: number;
+  recipe_id: number;
+  image_url: string;
+  text: string;
+};
