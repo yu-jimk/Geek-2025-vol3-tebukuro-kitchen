4ff26c896ea5cc2723fe8c97d5e18d11360b1717
watagassa <k23095kk@aitech.ac.jp>
2024-09-10 14:34:34 +0900

    add:初期設定

---
+++ src/app/conponents/tesx.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const tesx = () => {
+  return (
+    <div>tesx</div>
+  )
+}
+
+export default tesx
---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useEffect } from "react";
+// ここ大文字で
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mt-4 rounded shadow-md max-w-md mx-auto">
+      <h3 className="font-bold mb-2">エラー</h3>
+      <p>エラーが発生しました。</p>
+      <button
+    //   再読み込み
+        onClick={() => reset()}
+        className="bg-red-600 text-white mt-2 px-4 py-2 rounded hover:bg-red-500 transition ease-in-out duration-200"
+      >
+        もう一度試す
+      </button>
+    </div>
+  );
+};
+
+export default Error;
