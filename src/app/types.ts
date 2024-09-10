da00e8c0e57c006767b5d559e7ce3e123af67770
watagassa <k23095kk@aitech.ac.jp>
2024-09-10 15:02:06 +0900

    fix:supabaseテーブル変更

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,23 +2,26 @@
 //     id: number;
 //     title: string;
 //   }
+// レシピ
 export type Recipe = {
   id: number;
   name: string;
   image_url?: string;
-  time?: number; //料理時間
+  time?: string; //料理時間
   comment?: string;
   howmany: string; //何人前
 };
+// 材料
 export type Ingredient = {
   id: number;
   recipe_id: number;
   name: string;
   amount: string;
 };
+// 作り方説明
 export type Descript = {
   id: number;
   recipe_id: number;
-  image_url: string;
-  text: string;
+  image_url?: string;
+  text?: string;
 };
