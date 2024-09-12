c4cfff9856521c6e547e0f12d44d8b25ae91c503
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-09-12 19:44:35 +0900

    add:/future_recipe_idのheader部分の作成

--- src/app/future_recipe_id/page.tsx
+++ src/app/future_recipe_id/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { IoIosArrowBack } from "react-icons/io";
 
 const RecipeId = () => {
   return (
-    <div>RecipeId</div>
-  )
-}
+    <header className="relative flex items-center p-6 border-b border-gray-400 shadow-md">
+      <IoIosArrowBack className="absolute left-5 text-3xl" />
+      <div className="flex-1 text-center text-xl font-semibold">
+        基本のチャーハン
+      </div>
+    </header>
+  );
+};
 
-export default RecipeId
+export default RecipeId;
