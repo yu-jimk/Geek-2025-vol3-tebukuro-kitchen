7f5e0c9326790fda2edac795764c9d4b4a9d6d47
akki-256 <misonicomi5102.gmail.com>
2024-09-17 19:21:43 +0900

    add:アイコンを押すことで色が変化する機能を追加

--- src/app/conponents/Footer.tsx
+++ src/app/conponents/Footer.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+"use client"
+
 import { FiSearch } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
 import { CgFileDocument } from "react-icons/cg";
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+  const iconsize = 28;
+const currentPath = usePathname();
   return (
-    <div className=' bg-white border-t-2 h-14  border-black fixed bottom-0 w-full px-16 pt-2 flex justify-between
-    /*線を影みたいにするならshadow-inner shadow-black*/'>
-      <a className=''><FiSearch size={28}/></a>
-      <div className='justify-center'><CgFileDocument size={28}/></div>
-      <div className=''><AiOutlineUser size={28}/></div>
+    <div className='border-t-2 border-black w-full fixed bottom-0 h-14 pt-2 flex justify-center 
+    /shadow-inner shadow-black/'>
+      <Link className='w-1/3' href={'/'}>
+        <FiSearch size={iconsize} color={currentPath === '/'||'/favorites' ? 'orange' : 'gray'} />
+      </Link>
+      <Link className='w-1/3' href={'/registration'}>
+        <CgFileDocument size={iconsize} color={currentPath === '/registration' ? 'orange' : 'gray'} />
+      </Link>
+      <Link className='' href={'/future_user_id'}>
+        <AiOutlineUser size={iconsize} color={currentPath === '/future_user_id' ? 'orange' : 'gray'} />
+      </Link> 
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
