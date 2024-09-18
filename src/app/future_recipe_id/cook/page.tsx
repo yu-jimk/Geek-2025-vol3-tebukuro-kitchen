51332822d432c32a9e2e37f15d282c8f08f0ef19
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2024-09-18 17:39:58 +0900

    update: 文言仮置きを変更
    改行等の挙動を見るため

--- src/app/future_recipe_id/cook/page.tsx
+++ src/app/future_recipe_id/cook/page.tsx
@@ -9,15 +9,19 @@ const Cook = () => {
   return (
     <>
       <div className="bg-orange-400 h-20">Header置くとこ（サイズは適当）</div>
-      <div className="flex justify-center content-center">
-        <Image src="" alt="" width={400} height={400} className="shadow-md"/>
+      <div className="mx-10">
+        <div className="flex justify-center content-center">
+          <Image src="" alt="" width={400} height={400} className="shadow-md" />
+        </div>
+        <div className="mt-5 mb-10 flex justify-center">
+          <CgShapeCircle className="mx-1 text-black" />
+          <CgShapeCircle className="mx-1 text-black" />
+          <CgShapeCircle className="mx-1 text-black" />
+        </div>
+        <div className="font-bold text-center text-black text-3xl">
+          フライパンに油をひき、卵を割る。白身が白くなったらお米を入れる。
+        </div>
       </div>
-      <div className="mt-5 mb-10 flex justify-center">
-        <CgShapeCircle className="mx-1 text-black"/>
-        <CgShapeCircle className="mx-1 text-black"/>
-        <CgShapeCircle className="mx-1 text-black"/>
-      </div>
-      <div className="font-bold text-center text-black text-3xl">説明がここに入るよ</div>
     </>
   );
 };
