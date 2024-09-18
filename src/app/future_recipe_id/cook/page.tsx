7ef836a4ac7bc67a2b5bff50af849f2fa05a91a2
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2024-09-18 19:49:10 +0900

    update: ページ位置を示す丸の改良

--- src/app/future_recipe_id/cook/page.tsx
+++ src/app/future_recipe_id/cook/page.tsx
@@ -4,23 +4,32 @@ import React from "react";
 import Image from "next/image";
 import { CgShapeCircle } from "react-icons/cg";
 
-//データベースからの取得は後！！！
+//データベースからの取得は後。仮データ
+const page: number = 3; //ページ数
+
+const Circle = ({ count }: { count: number }) => {
+  return (
+    <>
+      {Array.from({ length: count }).map((_, index) => (
+        <div key={index} className="mx-2 w-2 h-2 border border-black rounded-full"></div>
+      ))}
+    </>
+  );
+};
+
 const Cook = () => {
   return (
     <>
       <div className="bg-orange-400 h-20">Header置くとこ（サイズは適当）</div>
-      <div className="mx-10">
-        <div className="flex justify-center content-center">
-          <Image src="" alt="" width={400} height={400} className="shadow-md" />
-        </div>
-        <div className="mt-5 mb-10 flex justify-center">
-          <CgShapeCircle className="mx-1 text-black" />
-          <CgShapeCircle className="mx-1 text-black" />
-          <CgShapeCircle className="mx-1 text-black" />
-        </div>
-        <div className="font-bold text-center text-black text-3xl">
-          フライパンに油をひき、卵を割る。白身が白くなったらお米を入れる。
-        </div>
+
+      <div className="flex justify-center content-center">
+        <Image src="" alt="" width={500} height={400} className="shadow-md" />
+      </div>
+      <div className="mt-6 mb-10 flex justify-center">
+        <Circle count={page}/>
+      </div>
+      <div className="mx-5 font-bold text-center text-black text-3xl">
+        フライパンに油をひき、卵を割る。白身が白くなったらお米を入れる。
       </div>
     </>
   );
