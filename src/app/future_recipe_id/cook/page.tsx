a7fbf2acaafb34580e55df2edd1068d2f040738d
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2024-09-18 20:10:05 +0900

    update: ページ位置示す丸の塗りつぶし処理

--- src/app/future_recipe_id/cook/page.tsx
+++ src/app/future_recipe_id/cook/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { CgShapeCircle } from "react-icons/cg";
 
 //データベースからの取得は後。仮データ
 const page: number = 3; //ページ数
 
-const Circle = ({ count }: { count: number }) => {
+const Circle = ({ count, id }: { count: number; id: number }) => {
   return (
     <>
       {Array.from({ length: count }).map((_, index) => (
-        <div key={index} className="mx-2 w-2 h-2 border border-black rounded-full"></div>
+        <div
+          key={index}
+          className={`mx-2 w-2 h-2 border border-black rounded-full ${
+            id == index ? "bg-orange-400" : ""
+          }`}
+        ></div>
       ))}
     </>
   );
 };
 
 const Cook = () => {
+  const [id, setId] = useState(0); //現在のページ
   return (
     <>
       <div className="bg-orange-400 h-20">Header置くとこ（サイズは適当）</div>
@@ -26,10 +31,25 @@ const Cook = () => {
         <Image src="" alt="" width={500} height={400} className="shadow-md" />
       </div>
       <div className="mt-6 mb-10 flex justify-center">
-        <Circle count={page}/>
+        <Circle count={page} id={id} />
       </div>
       <div className="mx-5 font-bold text-center text-black text-3xl">
         フライパンに油をひき、卵を割る。白身が白くなったらお米を入れる。
+      </div>
+
+      <div className="text-black flex justify-between">
+        <button
+          onClick={() => setId(id - 1)}
+          className="w-10 h-10 bg-orange-400"
+        >
+          前
+        </button>
+        <button
+          onClick={() => setId(id + 1)}
+          className="w-10 h-10 bg-orange-400"
+        >
+          次
+        </button>
       </div>
     </>
   );
