ee0994290380ffff0f03100ca9dca2b37d0013bd
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-09-22 16:40:05 +0900

    add:画像周りの実装

--- src/app/future_recipe_id/page.tsx
+++ src/app/future_recipe_id/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FiCameraOff } from "react-icons/fi";
 import { WiTime4 } from "react-icons/wi";
 import DescriptItem from "../conponents/DescriptItem";
 import FavoriteButton from "../conponents/FavoriteButton";
@@ -8,6 +9,8 @@ import IngredientItem from "../conponents/IngredientItem";
 import RecipeHeader from "../conponents/RecipeHeader";
 
 const RecipeId = () => {
+  const imageExist: boolean = false;
+
   return (
     <>
       <RecipeHeader
@@ -18,8 +21,19 @@ const RecipeId = () => {
       />
 
       <main className="bg-[#FFFBF4] pb-10">
-        <div className="flex justify-center border-b border-gray-400 shadow-md">
-          <Image src="/" alt="" width={430} height={297} />
+        <div className="flex justify-center items-center border-b border-gray-400 shadow-md aspect-[3/2] bg-gray-100">
+          {imageExist ? (
+            <Image
+              src="https://picsum.photos/300"
+              alt="基本のチャーハン"
+              width={430}
+              height={297}
+              layout={"responsive"}
+              objectFit={"cover"}
+            />
+          ) : (
+            <FiCameraOff size={40} stroke="#737373" />
+          )}
         </div>
 
         <div className="border-b border-gray-300 m-4">
@@ -40,23 +54,25 @@ const RecipeId = () => {
           </p>
         </div>
 
-        <div className="pt-1 pb-8">
-          <p className="bg-[#F9DEDC] font-semibold text-sm px-4 py-2">
-            材料（2人分）
-          </p>
-          <IngredientItem />
-        </div>
+        <div className="lg:flex justify-between items-center">
+          <div className="pt-1 pb-8 flex-1">
+            <p className="bg-[#F9DEDC] font-semibold text-sm px-4 py-2">
+              材料（2人分）
+            </p>
+            <IngredientItem />
+          </div>
 
-        <div className="mx-4">
-          <p className="font-semibold text-lg pb-1 mb-3 border-b border-black">
-            作り方
-          </p>
-          <DescriptItem />
+          <div className="mx-4">
+            <p className="font-semibold text-lg pb-1 mb-3 border-b border-black">
+              作り方
+            </p>
+            <DescriptItem />
+          </div>
         </div>
 
         <Link
           href="./future_recipe_id/cook"
-          className="flex justify-center text-white bg-orange-400 font-semibold rounded-xl text-lg py-3 w-64 shadow-md mx-auto mt-8"
+          className="flex justify-center text-white bg-orange-400 hover:bg-orange-500 font-semibold rounded-xl text-lg py-3 w-64 shadow-md mx-auto mt-8"
         >
           つくる
         </Link>
