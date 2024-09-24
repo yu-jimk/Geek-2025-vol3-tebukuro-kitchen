161095738aa8803ca1c987fa14c7fb559c17ee9d
watagassa <k23095kk@aitech.ac.jp>
2024-09-24 15:11:15 +0900

    update:add追加

--- src/app/utils/supabaseFunctions.ts
+++ src/app/utils/supabaseFunctions.ts
@@ -1,42 +1,75 @@
 import { PostgrestSingleResponse } from "@supabase/supabase-js";
 import { supabase } from "../utils/supabase";
-import { Descript, Ingredient } from "../types";
+import { Descript, Ingredient, Recipe } from "../types";
 
-// export const getAllTodos = async () => {
-//   const todos = await supabase.from("todo").select("*");
-//   return todos.data;
-// };
-
-// export const addTodo = async (title: string) => {
-//   await supabase.from("todo").insert({ title: title });
-// };
-// export const deleteTodo = async (id: number) => {
-//   await supabase.from("todo").delete().eq("id", id);
-// };
-
-
-export const getAllRecipes  = async () => {
+export const getAllRecipes = async () => {
   const recipes = await supabase.from("Recipes").select("*");
-  return recipes.data;
+  // 強制的にRecipe[]として認識させる
+  return recipes.data as Recipe[];
+};
+
+export const addRecipe = async (
+  name: string,
+  image_url?: string,
+  how_many?: string,
+  time?: string,
+  comment?: string
+) => {
+  await supabase.from("Recipes").insert({
+    name: name,
+    image_url: image_url,
+    how_many: how_many,
+    time: time,
+    comment: comment,
+  });
 };
 
 export const deleteRecipe = async (id: number) => {
   await supabase.from("Recipes").delete().eq("id", id);
 };
 
-export const getByIngredientId  = async (recipe_id:number) => {
-
-  const Ingredients:PostgrestSingleResponse<Ingredient[]> = await supabase.from("Ingredients").select("*").eq("recipe_id",recipe_id);
-  if(Ingredients.data === null){
+export const getByIngredientId = async (recipe_id: number) => {
+  const Ingredients: PostgrestSingleResponse<Ingredient[]> = await supabase
+    .from("Ingredients")
+    .select("*")
+    .eq("recipe_id", recipe_id);
+  if (Ingredients.data === null) {
     return -1;
   }
   return Ingredients.data;
 };
-export const  getByDescriptId = async (recipe_id:number) => {
 
-  const Descripts: PostgrestSingleResponse<Descript[]> = await supabase.from("Descripts").select("*").eq("recipe_id",recipe_id);
-  if(Descripts.data === null){
+export const addIngredient = async (
+  recipe_id: number,
+  name: string,
+  amount: string
+) => {
+  await supabase.from("Ingredients").insert({
+    recipe_id: recipe_id,
+    name: name,
+    amount: amount,
+  });
+};
+
+export const getByDescriptId = async (recipe_id: number) => {
+  const Descripts: PostgrestSingleResponse<Descript[]> = await supabase
+    .from("Descripts")
+    .select("*")
+    .eq("recipe_id", recipe_id);
+  if (Descripts.data === null) {
     return -1;
   }
   return Descripts.data;
+};
+
+export const addDescript = async (
+  recipe_id: number,
+  image_url?: string,
+  text?: string
+) => {
+  await supabase.from("Ingredients").insert({
+    recipe_id: recipe_id,
+    image_url: image_url,
+    text: text,
+  });
 };
