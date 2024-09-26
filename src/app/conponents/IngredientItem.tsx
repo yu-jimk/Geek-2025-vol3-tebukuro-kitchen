e5e369f5415ea8556b5e0f5025c18bd94b0f6105
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-09-26 11:53:03 +0900

    add:supabaseからレシピ詳細情報を取得し表示するようにした。

--- src/app/conponents/IngredientItem.tsx
+++ src/app/conponents/IngredientItem.tsx
@@ -1,8 +1,11 @@
-const IngredientItem = () => {
+import { Ingredient } from "@/app/types";
+
+const IngredientItem = (props: Ingredient) => {
+  const { name, amount } = props;
   return (
     <div className="flex items-center justify-between px-4 py-3 border-b border-[#C3B6B6] bg-[#FEF9EC]">
-      <p>たまご</p>
-      <p>２個</p>
+      <p>{name}</p>
+      <p>{amount}</p>
     </div>
   );
 };
