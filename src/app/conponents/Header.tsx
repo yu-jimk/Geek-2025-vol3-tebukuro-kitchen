17afd9160df46aeafcf5fe16551e406ab60d2dc6
akki-256 <misonicomi5102.gmail.com>
2024-10-01 16:46:15 +0900

    update:Footer,Headerの機能改善

--- src/app/conponents/Header.tsx
+++ src/app/conponents/Header.tsx
@@ -1,30 +1,27 @@
-'use client'
+type headerProp={
+    pathName : string
+}
 
-import React, { useState } from 'react'
-
-const Header = () => {
-    
-    const [selectbutton, setselectbutton] = useState('レシピ')
-
-
+const Header = (prop:headerProp) => {
+    const {pathName} = prop
   return (
-    <div className='border-b-2 border-black w-full p-3'>
+    <div className='bg-white border-b-2 border-black w-full p-3'>
 
-        <input className=' p-2 bg-gray-200 mt-3 mb-5 w-full text-2xl border-spacing- border border-gray-400 rounded-lg focus:outline-none' 
+        <input className=' p-2 bg-gray-200 mt-3 mb-5 w-full text-2xl border-spacing-4 border-gray-50 rounded-2xl focus:outline-none' 
         type='text' 
         placeholder= 'レシピをさがす' />
 
         <div className='flex'>
             <div className='w-1/2 px-4 flex justify-center '>
-                <button className={`px-4 py-2 ${selectbutton === 'レシピ' ? 'border-b-4 border-orange-500' : 'border-b-4 border-white'}`}
-                onClick={() => setselectbutton('レシピ')}>
+                <button className={`px-4 py-2 ${pathName === '/' ? 'border-b-4 border-orange-500' : 'border-b-4 border-white'}`}
+                onClick={() => location.href='/'}>
                     レシピ
                 </button>
             </div>
 
             <div className='w-1/2 px-4 flex justify-center '>
-                <button className={`px-4 py-2 ${selectbutton === 'お気に入り' ? 'border-b-4 border-orange-500' : 'border-b-4 border-white'}`}
-                onClick={()=> setselectbutton('お気に入り')}>
+                <button className={`px-4 py-2 ${pathName === '/favorites' ? 'border-b-4 border-orange-500' : 'border-b-4 border-white'}`}
+                onClick={()=> location.href='/favorites'}>
                     お気に入り
                 </button>
             </div>
