59300861f2eeec1bbcf4a80e4d9c3516e52b13cb
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-10-08 16:41:02 +0900

    update:データ取得方法を変更

--- src/app/[recipe_id]/page.tsx
+++ src/app/[recipe_id]/page.tsx
@@ -1,48 +1,59 @@
+"use client";
+
 import DescriptItem from "@/app/conponents/DescriptItem";
 import FavoriteButton from "@/app/conponents/FavoriteButton";
 import Footer from "@/app/conponents/Footer";
 import IngredientItem from "@/app/conponents/IngredientItem";
 import RecipeHeader from "@/app/conponents/RecipeHeader";
-import { Descript, Ingredient } from "@/app/types";
+import { Descript, DetailRecipe, Ingredient } from "@/app/types";
+import { getDetailRecipebyId } from "@/app/utils/supabaseFunctions";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { FiCameraOff } from "react-icons/fi";
 import { WiTime4 } from "react-icons/wi";
 
-export default async function RecipeId({
+export default function RecipeId({
   params,
 }: {
-  params: { recipe_id: string };
+  params: { recipe_id: number };
 }) {
-  // const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const API_URL = "http://localhost:3000";
+  const [list, setList] = useState<DetailRecipe>();
+  useEffect(() => {
+    const getDetailRecipe = async () => {
+      const detailRecipe = await getDetailRecipebyId(params.recipe_id);
+      setList(detailRecipe);
+      // const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  const res = await fetch(`${API_URL}/api/${params.recipe_id}`, {
-    next: {
-      revalidate: 10,
-    },
-  });
+      // const res = await fetch(`${API_URL}/api/${params.recipe_id}`, {
+      //   next: {
+      //     revalidate: 10,
+      //   },
+      // });
+    };
+    getDetailRecipe();
+  }, [params.recipe_id]);
 
-  const detailArticle = await res.json();
-
-  // console.log(detailArticle);
+  if (!list) {
+    return null;
+  }
 
   return (
     <>
       <RecipeHeader
         bgColor="bg-white"
         textColor="text-black"
-        title={detailArticle.name}
+        title={list.name}
         link="/"
       />
 
-      <main className="bg-[#FFFBF4] pb-10">
+      <main className="bg-[#FFFBF4] pb-10 min-h-[calc(100vh-150px)] ">
         <div className="flex justify-center items-center border-b border-gray-400 shadow-md aspect-[3/2] bg-gray-100">
           {/* nullのみを判定しているので、url先の画像が見つからない場合に対処できない */}
-          {detailArticle.image_url ? (
+          {list.image_url ? (
             <Image
-              src={detailArticle.image_url}
-              alt={detailArticle.name}
+              src={list.image_url}
+              alt={list.name}
               width={450}
               height={300}
               layout={"responsive"}
@@ -55,36 +66,39 @@ export default async function RecipeId({
 
         <div className="border-b border-gray-300 m-4 pb-4">
           <div className="flex justify-between">
-            <p className="text-2xl font-semibold text-[#815B2C]">
-              {detailArticle.name}
-            </p>
-            <FavoriteButton id={detailArticle.id} />
+            <p className="text-2xl font-semibold text-[#815B2C]">{list.name}</p>
+            <FavoriteButton
+              recipe={{
+                id: list.id,
+                name: list.name,
+                image_url: list.image_url,
+                time: list.time,
+                comment: list.comment,
+                howmany: list.howmany,
+              }}
+            />
           </div>
 
-          {detailArticle.time ? (
+          {list.time && (
             <div className="flex items-center py-2 gap-2">
               <WiTime4 fill="#fa003f" className="size-8" />
-              {detailArticle.time}
+              {list.time}
             </div>
-          ) : null}
+          )}
 
-          {detailArticle.comment ? (
+          {list.comment && (
             <p className="text-sm font-semibold text-stone-600 pt-3">
-              {detailArticle.comment}
+              {list.comment}
             </p>
-          ) : null}
+          )}
         </div>
 
-        <div className="lg:flex justify-between items-center">
+        <div className="lg:flex justify-between items-start">
           <div className="pt-1 pb-8 flex-1">
             <div className="bg-[#F9DEDC] font-semibold text-sm px-4 py-2">
-              {detailArticle.how_many ? (
-                <p>材料（{detailArticle.how_many}）</p>
-              ) : (
-                <p>材料</p>
-              )}
+              {list.howmany ? <p>材料（{list.howmany}）</p> : <p>材料</p>}
             </div>
-            {detailArticle.Ingredients.map((ingredient: Ingredient) => (
+            {list.Ingredients.map((ingredient: Ingredient) => (
               <IngredientItem
                 key={ingredient.id}
                 id={ingredient.id}
@@ -99,15 +113,17 @@ export default async function RecipeId({
             <p className="font-semibold text-lg pb-1 mb-3 border-b border-black">
               作り方
             </p>
-            {detailArticle.Descripts.map((descript: Descript) => (
-              <DescriptItem
-                key={descript.id}
-                id={descript.id}
-                text={descript.text}
-                image_url={descript.image_url}
-                recipe_id={descript.recipe_id}
-              />
-            ))}
+            <div className="space-y-1">
+              {list.Descripts.map((descript: Descript, index) => (
+                <DescriptItem
+                  key={descript.id}
+                  id={index + 1}
+                  text={descript.text}
+                  image_url={descript.image_url}
+                  recipe_id={descript.recipe_id}
+                />
+              ))}
+            </div>
           </div>
         </div>
 
