dd30bdeda5b6ea0f7f6dd6bfbd9af8533ab6360d
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-10-13 01:19:44 +0900

    update:デザインの変更

--- src/app/conponents/ArticleCard.tsx
+++ src/app/conponents/ArticleCard.tsx
@@ -1,57 +1,42 @@
-'use client'
-
-import Link from "next/link";
+import { Recipe } from "@/app/types";
 import Image from "next/image";
-import { Recipe } from "../types";
+import Link from "next/link";
+import { FiCameraOff } from "react-icons/fi";
 
-type prop_type ={
-  recipe? : Recipe;
-  id?:string
+type ArticleCardProps = {
+  recipe: Recipe;
 };
 
-  const ArticleCard = (prop:prop_type) => {
+const ArticleCard = (props: ArticleCardProps) => {
+  const { recipe } = props;
+  // const existImage: boolean = false;
+  // console.log("This`s ArticleCard");
 
-    const{recipe,id}=prop
-    console.log('This`s ArticleCard')
+  return (
+    <Link
+      href={`/${recipe.id}`}
+      className="border-[1px] border-gray-400 rounded-lg"
+    >
+      <div className="relative rounded-t-[7px] overflow-hidden aspect-[7/10] flex justify-center items-center bg-gray-100">
+        {recipe.image_url ? (
+          <Image
+            // src={`https://picsum.photos/${recipe.id + 500}`}
+            src={recipe.image_url}
+            alt={recipe.name}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="rounded-t-[7px] object-cover"
+          />
+        ) : (
+          <FiCameraOff size={30} stroke="#9ca3af" />
+        )}
+      </div>
 
-    if(recipe != null)
-      return (
-        <div className="py-3 px-4  bg-orange-100 " >
-              <Link href={`/${recipe.id}`}>
-              <div className="relative rounded-t-xl overflow-hidden"style={{ position: 'relative', aspectRatio: '4 / 5' }}>
-                  <Image 
-                      src={`/${recipe.image_url}`}
-                      layout="fill"
-                      objectFit="cover"
-                      alt=""
-                  />
-              </div>
-              <div className="h-24 border-t-4 border-orange-500 rounded-b-xl bg-white">
-                  {recipe.name}
-              </div>
-          </Link>
-        </div>
-      );
+      <p className="h-12 border-t-4 border-t-orange-400 rounded-b-lg bg-white font-semibold text-sm p-1">
+        {recipe.name}
+      </p>
+    </Link>
+  );
+};
 
-    if(id != null)
-      return (
-        <div className="py-3 px-4  bg-orange-100 " >
-              <Link href={`/${id}`}>
-              <div className="relative rounded-t-xl overflow-hidden"style={{ position: 'relative', aspectRatio: '4 / 5' }}>
-                  <Image 
-                      src={`/${null}`}
-                      layout="fill"
-                      objectFit="cover"
-                      alt=""
-                  />
-              </div>
-              <div className="h-24 border-t-4 border-orange-500 rounded-b-xl bg-white">
-                  {null}
-              </div>
-          </Link>
-        </div>
-      );
-    }
-  
-  
-  export default ArticleCard;
+export default ArticleCard;
