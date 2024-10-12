cbe3719bb4c1483ed3d2dfa9b150f3612f8c9fb3
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-10-13 01:04:23 +0900

    add:戻るボタンの色のpropsを追加
    /[recipe_id]と/cookでは色が異なるため

--- src/app/conponents/RecipeHeader.tsx
+++ src/app/conponents/RecipeHeader.tsx
@@ -6,16 +6,17 @@ type RecipeHeaderProps = {
   textColor: string;
   title: string;
   link: string;
+  iconFill: string;
 };
 
 const RecipeHeader = (props: RecipeHeaderProps) => {
-  const { bgColor, textColor, title, link } = props;
+  const { bgColor, textColor, title, link, iconFill } = props;
   return (
     <header
       className={`sticky top-0 w-full flex items-center p-6 border-b border-gray-400 shadow-md ${bgColor} z-50`}
     >
       <Link href={link} className="absolute left-5 text-3xl">
-        <IoIosArrowBack />
+        <IoIosArrowBack fill={iconFill} />
       </Link>
       <p className={`flex-1 text-center text-xl font-semibold ${textColor}`}>
         {title}
