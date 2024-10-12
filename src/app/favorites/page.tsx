6107aba18194f8a62c4b9e8e78eada3933824549
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-10-13 01:18:29 +0900

    add:お気に入り表示の実装

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -7,25 +7,43 @@ import { Recipe } from "@/app/types";
 import { getFavoriteRecipes } from "@/app/utils/localstorageFunction";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FiHeart } from "react-icons/fi";
 
 const Favorites = () => {
   const pathName = usePathname();
 
   const [list, setList] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const favoriteRecipes: Recipe[] = getFavoriteRecipes();
-    setList(favoriteRecipes);
+    if (favoriteRecipes.length > 0) setList(favoriteRecipes);
+    setLoading(false);
   }, []);
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header pathName={pathName} />
-      <div className="bg-orange-100 grid grid-cols-2">
-        {list.map((recipe: Recipe) => (
-          <ArticleCard recipe={recipe} key={recipe.id} />
-        ))}
-      </div>
+
+      {loading ? (
+        <div className="bg-[#FFFBF4] flex-grow"></div>
+      ) : list.length === 0 ? (
+        <div className="bg-[#FFFBF4] flex-grow flex flex-col justify-center items-center gap-4">
+          <FiHeart size={55} />
+          <p className="font-black text-2xl">
+            お気に入りを
+            <br />
+            登録しよう！
+          </p>
+        </div>
+      ) : (
+        <div className="bg-[#FFFBF4] flex-grow grid grid-cols-2 auto-rows-min gap-5 p-5">
+          {list.map((recipe: Recipe) => (
+            <ArticleCard recipe={recipe} key={recipe.id} />
+          ))}
+        </div>
+      )}
+
       <Footer pathName={pathName} />
     </div>
   );
