a6797f084103e3c5e6450667b567faea1da6dbd6
ranbudayo <mc_ryouta3@yahoo.co.jp>
2024-10-29 15:31:31 +0900

    update: ガイド画面見やすくした
    見やすくなった・・・はず

--- src/app/[recipe_id]/cook/GuideModal.tsx
+++ src/app/[recipe_id]/cook/GuideModal.tsx
@@ -22,31 +22,25 @@ const IngModal = ({ modalClose }: { modalClose: () => void }) => {
             <div className="font-bold mx-5 w-52 text-xl border-b-2 border-orange-400">
               音声認識ガイド
             </div>
-            <div className="mt-5 mb-1 bg-orange-100">
-              <div className="ml-5">
-                <div className="text-2xl font-bold">「進んで」</div>
-                次のページに進みます。
-              </div>
-              <div className="ml-5">
-                <div className="text-2xl font-bold">「戻って」</div>
-                前のページに戻ります。
-              </div>
-            </div>
-            <div className="mt-5 mb-1 bg-orange-100">
-              <div className="ml-5">
-                <div className="text-2xl font-bold">「材料は？」</div>
-                必要な材料を一覧表示します。
-              </div>
-              <div className="ml-5">
-                <div className="text-2xl font-bold">「～ってどうするの？」</div>
-                切り方等分からないことをYoutubeで検索し、動画を再生します。
-              </div>
-            </div>
-            <div className="mt-5 mb-10 bg-orange-100">
-              <div className="ml-5">
-                <div className="text-2xl font-bold">「閉じて」</div>
-                材料一覧や動画などの表示を閉じます。
-              </div>
+            <div className="mx-5 mt-5 mb-5">
+              <span className="font-bold text-lg text-orange-400">「進んで」</span>
+              　ページを進めます。
+              <br />
+              <span className="font-bold text-lg text-orange-400">「戻って」</span>
+              　ページを戻します。
+              <br />
+              <br />
+              <span className="font-bold text-lg text-orange-400">「材料は?」</span>
+              　必要な材料を一覧表示します。
+              <br />
+              <div className="font-bold text-lg text-orange-400">「~ってどうするの?」</div>
+              　切り方等をYoutubeで検索し、動画を再生します。
+              <br />
+              <div className="font-bold text-lg text-orange-400">「タイマーxxセット」</div>
+              　タイマーをxxの時間セットして、表示します。
+              <br />
+              <div className="font-bold text-lg text-orange-400">「閉じて」</div>
+              　材料一覧や動画などの表示を閉じます。
             </div>
           </div>
         </div>
