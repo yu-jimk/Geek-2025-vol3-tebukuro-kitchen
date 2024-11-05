f165d7d123fbb72bfc602d6d360392ce5d09a93c
ranbudayo <mc_ryouta3@yahoo.co.jp>
2024-11-05 15:16:28 +0900

    update: ガイドモーダルのデザイン変更

--- src/app/[recipe_id]/cook/GuideModal.tsx
+++ src/app/[recipe_id]/cook/GuideModal.tsx
@@ -22,25 +22,35 @@ const IngModal = ({ modalClose }: { modalClose: () => void }) => {
             <div className="font-bold mx-5 w-52 text-xl border-b-2 border-orange-400">
               音声認識ガイド
             </div>
-            <div className="mx-5 mt-5 mb-5">
-              <span className="font-bold text-lg text-orange-400">「進んで」</span>
-              　ページを進めます。
-              <br />
-              <span className="font-bold text-lg text-orange-400">「戻って」</span>
-              　ページを戻します。
-              <br />
-              <br />
-              <span className="font-bold text-lg text-orange-400">「材料は?」</span>
-              　必要な材料を一覧表示します。
-              <br />
-              <div className="font-bold text-lg text-orange-400">「~ってどうするの?」</div>
-              　切り方等をYoutubeで検索し、動画を再生します。
-              <br />
-              <div className="font-bold text-lg text-orange-400">「タイマーxxセットして」</div>
-              　タイマーをxxの時間セットして、表示します。
-              <br />
-              <div className="font-bold text-lg text-orange-400">「閉じて」</div>
-              　材料一覧や動画などの表示を閉じます。
+            <div className="mx-2 mt-5 mb-5">
+              <div className="font-bold text-lg text-orange-400">
+                「進んで」
+              </div>
+              <div className="ml-8">ページを進めます。</div>
+              <div className="font-bold text-lg text-orange-400">
+                「戻って」
+              </div>
+              <div className="ml-8">ページを戻します。</div>
+              <div className="font-bold text-lg text-orange-400">
+                「材料は?」
+              </div>
+              <div className="ml-8">必要な材料を一覧表示します。</div>
+              <div className="font-bold text-lg text-orange-400">
+                「~ってどうするの?」
+              </div>
+              <div className="ml-8">
+                切り方等をYoutubeで検索し、動画を再生します。
+              </div>
+              <div className="font-bold text-lg text-orange-400">
+                「タイマーxxセットして」
+              </div>
+              <div className="ml-8">
+                タイマーをxxの時間セットして、表示します。
+              </div>
+              <div className="font-bold text-lg text-orange-400">
+                「閉じて」
+              </div>
+              <div className="ml-8">材料一覧や動画などの表示を閉じます。</div>
             </div>
           </div>
         </div>
