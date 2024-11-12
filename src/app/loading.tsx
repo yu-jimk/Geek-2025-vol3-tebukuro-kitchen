a8817f55d154ccdba02a939c4a554ee81f86088e
haruya0630 <k23042kk@aitech.ac.jp>
2024-11-12 15:20:09 +0900

    add:マウント時のローディング実装

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center gap-6  bg-[#FFFBF4] h-screen">
+      <div className="h-10 w-10 animate-spin border-[5px] border-gray-400 rounded-full  border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Loading;
