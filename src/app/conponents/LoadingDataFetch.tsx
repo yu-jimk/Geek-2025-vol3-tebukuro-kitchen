db17ad10910bb26a386a4c45bea2f0cf12251570
haruya0630 <k23042kk@aitech.ac.jp>
2024-11-12 15:22:49 +0900

    add:データ取得時のローディング実装

---
+++ src/app/conponents/LoadingDataFetch.tsx
@@ -0,0 +1,9 @@
+const LoadingComponent = () => {
+  return (
+    <div className="flex justify-center items-center gap-6 bg-[#FFFBF4] flex-grow">
+      <div className="h-10 w-10 animate-spin border-[5px] border-gray-400 rounded-full  border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default LoadingComponent;
